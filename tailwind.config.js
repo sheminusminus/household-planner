e40609d92bce61872e48aef2536a25b6a0c393dc
Emily Kolar <emkolardev@gmail.com>
2026-01-07 00:02:58 -0600

    Update brand color to custom blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          500: '#1195dc',
+          600: '#0d7ab8',
+        }
+      }
+    },
   },
   plugins: [],
 }
